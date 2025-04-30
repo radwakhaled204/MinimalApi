373893e565bbfaaba17d34ab37470e14adbe31ae
Radwa Khaled <radwaabouzed@gmail.com>
2025-04-30 14:48:24 +0300

    add swagger services

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,16 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.InMemory;
-
 using MinimalApi.Data;
+using MinimalApi.Modeles;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("ProductsDb"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.MapGet("/products", async (AppDbContext db) => await db.products.ToListAsync());
+
+
+//app.MapGet("/products", async (AppDbContext db) => await db.products.ToListAsync());
 
 
 app.Run();
