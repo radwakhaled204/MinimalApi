4db136c39fd760f859d10acc8f86dea7b24da39d
Radwa Khaled <radwaabouzed@gmail.com>
2025-04-30 12:12:59 +0300

    set name equals empty To Avoid Non-nullable Warning

--- MinimalApi/Modeles/Product.cs
+++ MinimalApi/Modeles/Product.cs
@@ -3,6 +3,7 @@
     public class Product
     {
         public int Id { get; set; }
+        //To Avoid Non-nullable Warning
         public string Name { get; set; } = string.Empty;
 
         public decimal Price { get; set; }
