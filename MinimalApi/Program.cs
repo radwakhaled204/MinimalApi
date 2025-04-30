df88b8c5a10f82afaf2576a2f773cc9b03127d29
Radwa Khaled <radwaabouzed@gmail.com>
2025-04-30 16:08:20 +0300

    up

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -44,6 +44,8 @@ app.MapPut("/products/{id}", async (int id ,Product input, AppDbContext db) =>
     await db.SaveChangesAsync();
     return Results.Created($"{product.Id}", product);
 });
+
+//add delete
 app.MapDelete("/products/{id}", async (int id, AppDbContext db) =>
 {
     var product = await db.products.FindAsync(id);
