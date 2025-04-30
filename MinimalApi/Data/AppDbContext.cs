f9252d5a2c22f59df5318eadac84d30aa7f4e15d
Radwa Khaled <radwaabouzed@gmail.com>
2025-04-30 12:33:39 +0300

    add AppDbContext

--- MinimalApi/Data/AppDbContext.cs
+++ MinimalApi/Data/AppDbContext.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MinimalApi.Modeles;
 
 namespace MinimalApi.Data
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> products => Set <Product>();
+    
     }
 }
